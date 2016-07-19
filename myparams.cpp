cb440c3f805500679eeeab4ed722b762f4b5012b
Dr. Pascal Steger <accounts@steger.aero>
2016-07-19 14:58:11 +0000

    refactoring: myparams

---
+++ myparams.cpp
@@ -0,0 +1,40 @@
+#include <string>     /* std::cout */
+#include <iostream>
+#include <stdlib.h>   /* atof */
+#include "tests.hpp"
+#include "datastructures.hpp"
+#include "myoutput.hpp"
+#include "debug.hpp"
+
+param_struct parse_params( int argc, char* argv[]){
+  param_struct fill;
+  fill.Niterations = 1000;
+  fill.pois_cycles = 0.5;
+  // run through all possible runtime parameters with a loop
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if ((arg == "-h") || (arg == "--help")) {
+      show_usage(argv[0]);
+      exit(0);
+    }
+    else if ((arg == "-t") || (arg == "--test")) {
+      test_my_mkdir();
+      //test_timestamp();
+      //test_copy();
+      exit(0);
+    } else if ((arg == "-i") || (arg == "--iterations")) {
+      if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+        fill.Niterations = atoi(argv[++i]); // Increment 'i' so we don't get the argument as the next argv[i].
+      } else { // Uh-oh, there was no argument to the destination option.
+        std::cerr << "-i|--iterations option requires one argument." << std::endl;
+      }
+    } else if ((arg == "-c") || (arg == "--cycles")) {
+      if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+        fill.pois_cycles = atof(argv[++i]); // Increment 'i' so we don't get the argument as the next argv[i].
+      } else { // Uh-oh, there was no argument to the destination option.
+        std::cerr << "-c|--cycles option requires one argument." << std::endl;
+      }
+    }
+  }
+  DEBUG && std::cout << "Niterations = " << fill.Niterations << std::endl;
+  return fill;}
---
+++ myparams.hpp
@@ -0,0 +1,8 @@
+#ifndef __myparams__
+#define __myparams__
+
+#include "datastructures.hpp"
+
+param_struct parse_params( int argc, char* argv[]);
+
+#endif
